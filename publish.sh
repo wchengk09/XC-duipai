0a11dd851733acf734e34a72d7f199a83df11e5c
wchengk09 <wchengk09@163.com>
2024-10-12 21:38:51 +0800

    deleted unused files

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+make clean
 git add .
 git commit
 git push origin master -f
-tar zcvf XC-duipai-$(date "+%Y%m%d%H%M%S").tar.gz * --exclude=ncurses --exclude=readline --exclude=*.txt --exclude=rand --exclude=std --exclude=wa
+tar zcvf ../XC-duipai-$(date "+%Y%m%d%H%M%S").tar.gz * --exclude=ncurses --exclude=readline --exclude=*.txt --exclude=rand --exclude=std --exclude=wa
